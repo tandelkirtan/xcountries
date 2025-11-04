ffa9f08948b65c78ee3e4402514439ec3d31429d
tandelkirtan <kirtandel111@gmail.com>
2025-11-05 00:25:41 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -8,23 +8,26 @@ function App() {
 
   const [data, setData] = useState([]);
 
+  const fetchData = async () => {
+    try{  
+      const response = await fetch(COUNTRY_API);
+      const jsonData = await response.json();
+      setData(jsonData);  
+    }
+    catch(err)
+    {
+      console.error("Error fetching data", err);
+    }
+  }
+
   useEffect(() => {
-    fetch(COUNTRY_API)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setData(data);
-      })
-      .catch((err) => {
-        console.error("Error fetching country data:", err);
-      });
-      
+      fetchData();
   }, [])
   
   return (
     <div className="App">
-      {data.map(({name, flag, abbr}) => (
-          <Cards name={name} flag={flag} abbr={abbr}/>
+      {data.map(({name, flag, abbr}, index) => (
+          <Cards key={index} name={name} flag={flag} abbr={abbr}/>
       ))}
     </div>
   );
